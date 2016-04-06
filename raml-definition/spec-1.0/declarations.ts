2241a602fee4f9af17c43b7c6f0a6170ae8a37d4
Christian Vogel <christianvogel84@gmail.com>
2016-04-06 15:16:11 +0100

    cleanup of the actual definition system ts files

--- raml-definition/spec-1.0/declarations.ts
+++ raml-definition/spec-1.0/declarations.ts
@@ -1,132 +1,131 @@
 import  MetaModel = require("../metamodel")
 import  Sys = require("./systemTypes")
-import  datamodel=require("./datamodel")
-import  common=require("./common")
-/////////////////////////////////
-// GENERIC GLOBAL DECLARATIONS
+import  DataModel=require("./datamodel")
+import  Common=require("./common")
 
-export class AnnotationTypeDeclaration extends datamodel.TypeDeclaration implements Sys.DeclaresDynamicType<AnnotationTypeDeclaration>{
+export class AnnotationTypeDeclaration extends DataModel.TypeDeclaration implements Sys.DeclaresDynamicType<AnnotationTypeDeclaration> {
 
-    allowedTargets:AnnotationTarget[]
-    $allowedTargets=[
-        MetaModel.oneOf(
-            [
-                "API",
-                "DocumentationItem",
-                "Resource",
-                "Method",
-                "Response",
-                "RequestBody",
-                "ResponseBody",
-                "TypeDeclaration",
-                "NamedExample",
-                "ResourceType",
-                "Trait",
-                "SecurityScheme",
-                "SecuritySchemeSettings",
-                "AnnotationTypeDeclaration",
-                "Library",
-                "Overlay",
-                "Extension"
-            ]
+  //On the design level every annotation usage is instantiation of subclass of particular AnnotationTypeDeclaration
+  //on the runtime level it is just Annotation (which is abstract on  the design level)
+  //this inheritance strangeness happens because we do not want bring AnnotationTypeDeclaration fields to Annotation
+  //TODO think about it
+  $=[
+    MetaModel.declaresSubTypeOf("Annotation"),
+    MetaModel.declaresSubTypeOf("AnnotationTypeDeclaration")
+  ]
 
-        ),
-        MetaModel.description("Restrictions on where annotations of this type can be applied. If this property is specified, annotations of this type may only be applied on a property corresponding to one of the target names specified as the value of this property."),
-        MetaModel.valueDescription("An array of names (or a single name) from the list of Target Names in the [[raml-10-spec-target-locations-table|Target Locations table]]  below. ")
-    ]
+  allowedTargets:AnnotationTarget[]
+  $allowedTargets=[
+    MetaModel.oneOf([
+      "API",
+      "DocumentationItem",
+      "Resource",
+      "Method",
+      "Response",
+      "RequestBody",
+      "ResponseBody",
+      "TypeDeclaration",
+      "NamedExample",
+      "ResourceType",
+      "Trait",
+      "SecurityScheme",
+      "SecuritySchemeSettings",
+      "AnnotationTypeDeclaration",
+      "Library",
+      "Overlay",
+      "Extension"
+    ]),
+    MetaModel.description("Restrictions on where annotations of this type can be applied. If this property is specified, " +
+      "annotations of this type may only be applied on a property corresponding to one of the target names specified as the " +
+      "value of this property."),
+    MetaModel.valueDescription("An array, or single, of names allowed target nodes.")
+  ]
 
-    usage: string
-    $usage = [
-        MetaModel.description("Instructions on how and when to use this annotation in a RAML spec."),
-        MetaModel.valueDescription("Markdown string")
-    ]
-
-    //On the design level every annotation usage is instantiation of subclass of particular AnnotationTypeDeclaration
-    //on the runtime level it is just Annotation (which is abstract on  the design level)
-    //this inheritance strangeness happens because we do not want bring AnnotationTypeDeclaration fields to Annotation
-    //TODO think about it
-    $=[
-        MetaModel.declaresSubTypeOf("Annotation"),
-        MetaModel.declaresSubTypeOf("AnnotationTypeDeclaration")
-    ]
+  usage:string
+  $usage=[
+    MetaModel.description("Instructions on how and when to use this annotation in a RAML spec."),
+    MetaModel.valueDescription("Markdown string")
+  ]
 }
-export class AnnotationRef extends Sys.Reference<AnnotationTypeDeclaration>{
-    // TODO: this should have more info on Annotations and their usage
-    $=[
-        MetaModel.description("Annotations allow you to attach information to your API"),
-        MetaModel.tags(['annotations'])
-    ]
 
-    annotation:AnnotationTypeDeclaration
-    $annotation = [MetaModel.customHandling(),MetaModel.description("Returns referenced annotation")]
+export class AnnotationRef extends Sys.Reference<AnnotationTypeDeclaration> {
+  // TODO: this should have more info on Annotations and their usage
+  $=[
+    MetaModel.description("Annotations allow you to attach information to your API"),
+    MetaModel.tags(['annotations'])
+  ]
+
+  annotation:AnnotationTypeDeclaration
+  $annotation=[
+    MetaModel.customHandling(),
+    MetaModel.description("Returns referenced annotation")
+  ]
 }
 
 export class AnnotationTarget extends Sys.ValueType{
-    $=[
-        // TODO: enum
-        MetaModel.description("Elements to which this Annotation can be applied (enum)"),
-        MetaModel.tags(['annotations'])
+  $=[
+    // TODO: enum
+    MetaModel.description("Elements to which this Annotation can be applied (enum)"),
+    MetaModel.tags(['annotations'])
+  ]
+}
+
+//This type does not exist on RAML design level (basically it's design level counter part is AnnotationRef)
+export class Annotation<T> {
+  name:string
+  $name=[MetaModel.key()]
+}
+
+export class ArrayAnnotationTypeDeclaration extends DataModel.ArrayTypeDeclaration {
+  $ = [
+    MetaModel.superclasses(["AnnotationTypeDeclaration"])
+  ]
+}
+
+export class UnionAnnotationTypeDeclaration extends DataModel.UnionTypeDeclaration {
+  $ = [
+    MetaModel.superclasses(["AnnotationTypeDeclaration"])
+  ]
+}
+
+export class ObjectAnnotationTypeDeclaration extends DataModel.ObjectTypeDeclaration {
+  $ = [
+    MetaModel.superclasses(["AnnotationTypeDeclaration"])
+  ]
+}
+
+export class StringAnnotationTypeDeclaration extends DataModel.StringTypeDeclaration {
+    $ = [
+      MetaModel.superclasses(["ValueAnnotationTypeDeclaration"])
     ]
 }
 
-
-
-//This type does not exist on RAML design level (basically it's design level counter part is AnnotationRef)
-export class Annotation<T>{
-    name:string
-
-    $name=[MetaModel.key()]
+export class BooleanAnnotationTypeDeclaration extends DataModel.BooleanTypeDeclaration {
+  $ = [
+    MetaModel.superclasses(["ValueAnnotationTypeDeclaration"])
+  ]
 }
 
-export class ArrayAnnotationTypeDeclaration extends datamodel.ArrayTypeDeclaration {
-
-    $ = [ MetaModel.superclasses(["AnnotationTypeDeclaration"]) ];
-
+export class ValueAnnotationTypeDeclaration extends DataModel.ValueTypeDeclaration {
+  $ = [
+    MetaModel.superclasses(["AnnotationTypeDeclaration"])
+  ]
 }
 
-export class UnionAnnotationTypeDeclaration extends datamodel.UnionTypeDeclaration {
-
-    $ = [ MetaModel.superclasses(["AnnotationTypeDeclaration"]) ];
+export class NumberAnnotationTypeDeclaration extends DataModel.NumberTypeDeclaration {
+  $ = [
+    MetaModel.superclasses(["ValueAnnotationTypeDeclaration"])
+  ]
 }
 
-export class ObjectAnnotationTypeDeclaration extends datamodel.ObjectTypeDeclaration {
-
-    $ = [ MetaModel.superclasses(["AnnotationTypeDeclaration"]) ];
-
+export class RAMLExpressionAnnotation extends DataModel.RAMLExpression {
+  $ = [
+    MetaModel.superclasses(["AnnotationTypeDeclaration"])
+  ]
 }
 
-export class StringAnnotationTypeDeclaration extends datamodel.StringTypeDeclaration {
-
-    $ = [ MetaModel.superclasses(["ValueAnnotationTypeDeclaration"]) ];
-
-}
-
-export class BooleanAnnotationTypeDeclaration extends datamodel.BooleanTypeDeclaration {
-
-    $ = [ MetaModel.superclasses(["ValueAnnotationTypeDeclaration"]) ];
-
-}
-
-export class ValueAnnotationTypeDeclaration extends datamodel.ValueTypeDeclaration {
-
-    $ = [ MetaModel.superclasses(["AnnotationTypeDeclaration"]) ];
-
-}
-
-export class NumberAnnotationTypeDeclaration extends datamodel.NumberTypeDeclaration {
-
-    $ = [ MetaModel.superclasses(["ValueAnnotationTypeDeclaration"]) ];
-
-}
-
-export class RAMLExpressionAnnotation extends datamodel.RAMLExpression {
-
-    $ = [ MetaModel.superclasses(["AnnotationTypeDeclaration"]) ];
-
-}
-
-export class DateTypeAnnotationDeclaration extends datamodel.DateTypeDeclaration {
-
-    $ = [ MetaModel.superclasses(["AnnotationTypeDeclaration"]) ];
-
+export class DateTypeAnnotationDeclaration extends DataModel.DateTypeDeclaration {
+  $ = [
+    MetaModel.superclasses(["AnnotationTypeDeclaration"])
+  ]
 }
--- raml-definition/spec-1.0/systemTypes.ts
+++ raml-definition/spec-1.0/systemTypes.ts
@@ -1,176 +1,152 @@
-import  MetaModel = require("../metamodel")
-import datamodel = require("./datamodel")
+import MetaModel = require("../metamodel")
+import DataModel = require("./datamodel")
+
 export class ValueType {
-    /**
-     * parses inner structure of value type if value type has invalid value you should throw error
-     * with descriptive message
-     */
-    parse():any{}
-
-
+  /**
+   * parses inner structure of value type if value type has invalid value you should throw error
+   * with descriptive message
+   */
+  parse():any{}
 }
-export class StringType extends ValueType { $=[MetaModel.nameAtRuntime("string"),MetaModel.alias("string")]; value():string{return null} }
-export class AnyType extends ValueType { $=[MetaModel.nameAtRuntime("any"),MetaModel.alias("any")]; value():any{return null} }
-export class NumberType extends ValueType { $=[MetaModel.nameAtRuntime("number"),MetaModel.alias("integer"),MetaModel.alias("number")]}
-export class BooleanType extends ValueType{ $=[MetaModel.nameAtRuntime("boolean"),MetaModel.alias("boolean")]}//FIXME
+
+export class StringType extends ValueType {
+  $=[
+    MetaModel.nameAtRuntime("string"),
+    MetaModel.alias("string")
+  ]
+
+  value():string {
+    return null
+  }
+}
+
+export class AnyType extends ValueType {
+  $=[
+    MetaModel.nameAtRuntime("any"),
+    MetaModel.alias("any")
+  ]
+
+  value():any {
+    return null
+  }
+}
+
+export class NumberType extends ValueType {
+  $=[
+    MetaModel.nameAtRuntime("number"),
+    MetaModel.alias("integer"),
+    MetaModel.alias("number")
+  ]
+}
+
+export class BooleanType extends ValueType {
+  $=[
+    MetaModel.nameAtRuntime("boolean"),
+    MetaModel.alias("boolean")
+  ]
+}//FIXME
+
 /**
  * Tag interface, types implementing this interface
  * are counted as global declarations, and their
  * instances may be referred
  */
 export interface Referencable<T>{}
-export class Reference<T>  extends ValueType{
-    structuredValue: datamodel.TypeInstance
-    $structuredValue = [MetaModel.customHandling(),MetaModel.description("Returns a structured object if the reference point to one.")]
 
-    name:string
-    $name = [MetaModel.customHandling(),MetaModel.description("Returns name of referenced object")]
+export class Reference<T>  extends ValueType {
+  structuredValue: DataModel.TypeInstance
+  $structuredValue=[
+    MetaModel.customHandling(),
+    MetaModel.description("Returns a structured object if the reference point to one.")
+  ]
+
+  name:string
+  $name=[
+    MetaModel.customHandling(),
+    MetaModel.description("Returns name of referenced object")
+  ]
 }//this is not true ...FIXME
 
 export interface DeclaresDynamicType<T> extends Referencable<T>{}//For now your still required to put declaresSubtype or inlinedTemplates annotation on the classs
 
 export class UriTemplate extends StringType {
+  $=[
+    MetaModel.description("This type currently serves both for absolute and relative urls")
+  ]
+}
 
+export class StatusCodeString extends StringType {}
+
+export class RelativeUriString extends UriTemplate {
     $=[
-        MetaModel.description("This type currently serves both for absolute and relative urls")
+      MetaModel.description("This  type describes relative uri templates")
     ]
-
-
-}
-export class StatusCodeString extends StringType{
-
 }
 
+export class FullUriTemplateString extends UriTemplate {
+  $=[
+    MetaModel.description("This  type describes absolute uri templates")
+  ]
 
-export class RelativeUriString extends UriTemplate{
-    $=[MetaModel.description("This  type describes relative uri templates")]
+  validate(){
+    var str=this.value();
+    //write something to validate Url here
+  }
+}
 
-    //parse():string[]{
-    //    var value=this.value();
-    //    var result=[]
-    //    var temp="";
-    //    var inPar=false;
-    //    var count=0;
-    //    for (var a=0;a<value.length;a++){
-    //        var c=value[a];
-    //        if (c=='{'){
-    //            count++;
-    //            inPar=true;
-    //            continue;
-    //        }
-    //        if (c=='}'){
-    //            count--;
-    //            inPar=false;
-    //            result.push(temp);
-    //            temp="";
-    //            continue;
-    //        }
-    //        if (inPar){
-    //            temp+=c;
-    //        }
-    //    }
-    //    if (count>0){
-    //        throw new Error("Unmatched '{'")
-    //    }
-    //    if (count<0){
-    //        throw new Error("Unmatched '}'")
-    //    }
-    //    return result;
-    //}
+export class FixedUriString extends StringType {
+  $=[
+    MetaModel.description("This  type describes fixed uris")
+  ]
 }
-export class FullUriTemplateString extends UriTemplate{
-    $=[MetaModel.description("This  type describes absolute uri templates")]
-    //parse():string[]{
-    //    var value=this.value();
-    //    var result=[]
-    //    var temp="";
-    //    var inPar=false;
-    //    var count=0;
-    //    for (var a=0;a<value.length;a++){
-    //        var c=value[a];
-    //        if (c=='{'){
-    //            count++;
-    //            inPar=true;
-    //            continue;
-    //        }
-    //        if (c=='}'){
-    //            count--;
-    //            inPar=false;
-    //            result.push(temp);
-    //            temp="";
-    //            continue;
-    //        }
-    //        if (inPar){
-    //            temp+=c;
-    //        }
-    //    }
-    //    if (count>0){
-    //        throw new Error("Unmatched '{'")
-    //    }
-    //    if (count<0){
-    //        throw new Error("Unmatched '}'")
-    //    }
-    //    return result;
-    //}
 
-    validate(){
-        var str=this.value();
-        //write something to validate Url here
-    }
-}
-export class FixedUriString extends StringType{
-    $=[MetaModel.description("This  type describes fixed uris")]
-}
-export class ContentType extends StringType{
+export class ContentType extends StringType {}
 
-}
-export class ValidityExpression extends StringType{
+export class ValidityExpression extends StringType {
     //TODO SPECIFY WHAT CAN NOT BE HERE
 }
 
-
 export class MarkdownString extends  StringType{
-    $=[
-        MetaModel.innerType("markdown"),
-        MetaModel.description("[GitHub Flavored Markdown](https://help.github.com/articles/github-flavored-markdown/)")
-    ]
+  $=[
+    MetaModel.innerType("markdown"),
+    MetaModel.description("[GitHub Flavored Markdown](https://help.github.com/articles/github-flavored-markdown/)")
+  ]
 }
-export class DateFormatSpec extends StringType{
+export class DateFormatSpec extends StringType {}
 
-}
-export class FunctionalInterface extends StringType{
+export class FunctionalInterface extends StringType {}
 
-}
+export class SchemaString extends StringType {
+  $=[MetaModel.description("Schema at this moment only two subtypes are supported (json schema and xsd)"),MetaModel.alias("schema")]
 
-export class SchemaString extends StringType{
-
-    $=[MetaModel.description("Schema at this moment only two subtypes are supported (json schema and xsd)"),MetaModel.alias("schema")]
-
-    validate(){
-        var str=this.value();
-        //write something to validate schema here here
-        //in fact it should check that content is valid json or xsd schema
-    }
-}
-export class ExampleString extends StringType{
-    $=[MetaModel.description("Examples at this moment only two subtypes are supported (json  and xml)")]
-
-    validate(){
-        var str=this.value();
-        //write something to validate schema here here
-        //in fact it should check that content is valid json or xsd schema
-    }
+  validate(){
+      var str=this.value();
+      //write something to validate schema here here
+      //in fact it should check that content is valid json or xsd schema
+  }
 }
 
-export class JSonSchemaString extends SchemaString{
-    $=[MetaModel.innerType("json"),MetaModel.description("JSON schema")]
+export class ExampleString extends StringType {
+  $=[MetaModel.description("Examples at this moment only two subtypes are supported (json  and xml)")]
+
+  validate(){
+    var str=this.value();
+    //write something to validate schema here here
+    //in fact it should check that content is valid json or xsd schema
+  }
+}
+
+export class JSonSchemaString extends SchemaString {
+  $=[
+    MetaModel.innerType("json"),
+    MetaModel.description("JSON schema")
+  ]
 }
 export class XMLSchemaString extends SchemaString{
-    $=[MetaModel.innerType("xsd"),MetaModel.description("XSD schema")]
+  $=[
+    MetaModel.innerType("xsd"),
+    MetaModel.description("XSD schema")
+  ]
 }
 
-
-
-export class RAMLSelector extends StringType{
-
-}
+export class RAMLSelector extends StringType{}
